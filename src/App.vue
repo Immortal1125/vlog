8857d63a4013b68aa48409022fc88e09a8373ab8
immort <immort521@gmail.com>
2023-11-14 14:20:07 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <Nav></Nav>
   <Content></Content>
   <RouterView></RouterView>
+  <AI></AI>
 </template>
 
 <script setup>
 import Nav from "./components/Nav/index.vue";
 import { RouterView } from "vue-router";
 import Content from "./components/content/index.vue";
+import AI from "./views/AI/index.vue";
 </script>
 
 <style scoped>
