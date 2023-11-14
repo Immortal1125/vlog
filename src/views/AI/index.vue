196066cb8705ab987198d8b156b00abbdce43c0c
immort <immort521@gmail.com>
2023-11-14 17:23:39 +0800

    modified:   src/views/AI/index.vue

--- src/views/AI/index.vue
+++ src/views/AI/index.vue
@@ -1,20 +1,5 @@
 <template></template>
 
 <script setup>
-import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: "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",
-});
-
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
-
-  console.log(completion.choices[0]);
-}
-
-main();
 </script>
