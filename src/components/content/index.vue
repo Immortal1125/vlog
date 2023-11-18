0bf93ef4b4ee77301af9d202b84f6ce06af70dd8
immort <immort521@gmail.com>
2023-11-18 22:31:11 +0800

    modified:   src/components/content/index.vue

--- src/components/content/index.vue
+++ src/components/content/index.vue
@@ -1,15 +1,1 @@
-<template>
-  <div class="content">
-    <Aside></Aside>
-    <Main></Main>
-  </div>
-</template>
-
-<script setup>
-import Aside from "../aside/index.vue";
-import Main from "../main/index.vue";
-</script>
-
-<style scoped>
-@import "./index.css";
-</style>
+<template></template>
